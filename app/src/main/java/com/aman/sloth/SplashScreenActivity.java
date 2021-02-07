4db7cec958038fb56689be293079e367f7b5db94
Aman Toppo <amntoppo@gmail.com>
2021-02-07 12:21:13 +0530

    Authentication added

--- app/src/main/java/com/aman/sloth/SplashScreenActivity.java
+++ app/src/main/java/com/aman/sloth/SplashScreenActivity.java
@@ -7,25 +7,82 @@ import io.reactivex.functions.Action;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private final static int LOGIN_REQUEST_CODE = 1337;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        displaySplashScreen();
+        init();
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if(firebaseAuth != null && listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+
+    }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser firebaseUser = myFirebaseAuth.getCurrentUser();
+            if(firebaseUser != null) {
+                displaySplashScreen();
+            }
+            else {
+                showLoginLayout();
+            }
+        };
+
+    }
+
+    private void showLoginLayout() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.layout_signin)
+                .setPhoneButtonId(R.id.button_phone_signin)
+                .setGoogleButtonId(R.id.button_email_signin)
+                .build();
+        startActivityForResult(AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAuthMethodPickerLayout(authMethodPickerLayout)
+        .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+        .setAvailableProviders(providers)
+        .build(), LOGIN_REQUEST_CODE);
+    }
+
     private void displaySplashScreen() {
 
         Completable.timer(5, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
-                .subscribe(new Action() {
-                    @Override
-                    public void run() throws Exception {
+                .subscribe(() -> {
 
-                    }
                 });
     }
 
