978e1a05f56477c309002c550d7f4a51231bd226
Aman Toppo <amntoppo@gmail.com>
2021-02-08 19:27:53 +0530

    Registration layout added

---
+++ app/src/main/java/com/aman/sloth/Model/CustomerInfoModel.java
@@ -0,0 +1,34 @@
+package com.aman.sloth.Model;
+
+public class CustomerInfoModel {
+    private String firstname, lastname, phoneNumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public CustomerInfoModel() {
+
+    }
+
+}
--- app/src/main/java/com/aman/sloth/SplashScreenActivity.java
+++ app/src/main/java/com/aman/sloth/SplashScreenActivity.java
@@ -1,18 +1,30 @@
 package com.aman.sloth;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Action;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.aman.sloth.Model.CustomerInfoModel;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +32,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,6 +50,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
     private ProgressBar progressBar;
+    CustomerInfoModel model;
 
 
 
@@ -66,7 +81,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        customerDatabaseRef = firebaseDatabase.getReference(Common.CUSTOMER_INFO_REFERENCE)
+        customerDatabaseRef = firebaseDatabase.getReference(Common.CUSTOMER_INFO_REFERENCE);
 
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -108,6 +123,56 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     private void showRegisterLayout() {
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null);
+
+        TextInputEditText edit_first_name = (TextInputEditText)itemView.findViewById(R.id.edit_first_name);
+        TextInputEditText edit_last_name = (TextInputEditText)itemView.findViewById(R.id.edit_last_name);
+        TextInputEditText edit_phone = (TextInputEditText)itemView.findViewById(R.id.edit_phone_number);
+        Button button_continue = (Button)itemView.findViewById(R.id.button_continue);
+
+        //set phone number
+        if(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber() != null &&
+                !TextUtils.isEmpty(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber())) {
+            edit_phone.setText(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+        }
+
+        //set view
+        builder.setView(itemView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        button_continue.setOnClickListener(v -> {
+            if(TextUtils.isEmpty(edit_first_name.getText().toString())) {
+                Toast.makeText(this, "Please enter first name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if(TextUtils.isEmpty(edit_last_name.getText().toString())) {
+                Toast.makeText(this, "Please enter last name", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if(TextUtils.isEmpty(edit_phone.getText().toString())) {
+                Toast.makeText(this, "Please enter phone number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else {
+                model = new CustomerInfoModel();
+                model.setFirstname(edit_first_name.getText().toString());
+                model.setLastname(edit_last_name.getText().toString());
+                model.setPhoneNumber(edit_phone.getText().toString());
+            }
+
+            customerDatabaseRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .setValue(model)
+                    .addOnFailureListener(e -> {
+                        dialog.dismiss();
+                        Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    })
+            .addOnSuccessListener(aVoid -> {
+                dialog.dismiss();
+                Toast.makeText(this, "Registration Success", Toast.LENGTH_SHORT).show();
+            });
+
+        });
 
     }
 
@@ -136,4 +201,18 @@ public class SplashScreenActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == LOGIN_REQUEST_CODE) {
+            Intent intent;
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+            else {
+                Toast.makeText(this, "Failed to sign in : " + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
