0b5e2b6e3866d08ed1b2e30ffa41c9018794c133
Aman Toppo <amntoppo@gmail.com>
2021-03-03 09:01:44 +0530

    button updation

--- app/src/main/java/com/aman/sloth/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/aman/sloth/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.aman.sloth.ui.gallery;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -38,15 +40,21 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements View.OnClickListener{
 
     private GalleryViewModel galleryViewModel;
     private TextView fadingText;
+
     //private TextView nameText;
     int count = 0;
 
     private RelativeLayout relativeLayout;
     private ConstraintLayout layoutHeader;
+
+    private LinearLayout layouyOrderHome;
+    private  LinearLayout layoutDeliverAnything;
+    private LinearLayout layoutDeliverYourself;
+    private LinearLayout layoutCreateShop;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -54,7 +62,10 @@ public class GalleryFragment extends Fragment {
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+        initializeButtons(root);
+
         //customer_first_name = Common.currentCustomer.getFirstname();
+
 
         fadingText = root.findViewById(R.id.hello_text);
         //nameText = root.findViewById(R.id.text_name);
@@ -104,5 +115,30 @@ public class GalleryFragment extends Fragment {
             }
         });
         return root;
+    }
+
+
+    private void initializeButtons(View root) {
+        layouyOrderHome = root.findViewById(R.id.layout_order_home);
+        layoutDeliverAnything = root.findViewById(R.id.layout_deliver_anything);
+        layoutDeliverYourself = root.findViewById(R.id.layout_get_yourself_delivered);
+        layoutCreateShop = root.findViewById(R.id.layout_create_shop);
+
+        layouyOrderHome.setOnClickListener(this);
+        layoutDeliverAnything.setOnClickListener(this);
+        layoutDeliverYourself.setOnClickListener(this);
+        layoutCreateShop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layout_order_home:
+                Log.e("button", "order home");
+                break;
+            case R.id.layout_deliver_anything:
+                Log.e("button", "order anything");
+                break;
+        }
     }
 }
