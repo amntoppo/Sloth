3b158b1ca3b22bd45434968e46bce4d3c5411e61
Aman Toppo <amntoppo@gmail.com>
2021-03-03 20:48:11 +0530

    Create shop map added

--- app/src/main/java/com/aman/sloth/ui/shop/CreateShopActivity.java
+++ app/src/main/java/com/aman/sloth/ui/shop/CreateShopActivity.java
@@ -1,31 +1,141 @@
 package com.aman.sloth.ui.shop;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import com.aman.sloth.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.internal.bind.ArrayTypeAdapter;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class CreateShopActivity extends AppCompatActivity {
+public class CreateShopActivity extends AppCompatActivity implements OnMapReadyCallback{
 
-    Spinner spinner;
+    private GoogleMap mmap;
+    private SupportMapFragment mapFragment;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_create_shop_register);
-        spinnerInitialize();
+        mapInitialize();
     }
 
-    private void spinnerInitialize() {
-        spinner = findViewById(R.id.order_home_spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateShopActivity.this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.itemselect));
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+    private void mapInitialize() {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_create_shop);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        locationRequest = new LocationRequest();
+        locationRequest.setSmallestDisplacement(10f);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LatLng newPosition = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 18f));
+            }
+        };
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mmap = googleMap;
+
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        mmap.setMyLocationEnabled(true);
+                        mmap.getUiSettings().setMyLocationButtonEnabled(true);
+
+                        mmap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                            @Override
+                            public boolean onMyLocationButtonClick() {
+                                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                    return false;
+                                }
+                                fusedLocationProviderClient.getLastLocation()
+                                        .addOnFailureListener(e -> Snackbar.make(findViewById(android.R.id.content), e.getMessage(), Snackbar.LENGTH_SHORT).show())
+                                        .addOnSuccessListener(location -> {
+                                            LatLng userLatlng = new LatLng(location.getLatitude(),location.getLongitude() );
+                                            mmap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatlng, 18f));
+                                        });
+                                return true;
+                            }
+                        });
+
+                        //location button
+                        View locationButton = ((View)mapFragment.getView().findViewById(Integer.parseInt("1")).getParent())
+                                .findViewById(Integer.parseInt("2"));
+                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+                        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+                        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                        params.setMargins(0, 0, 0, 50);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                })
+                .check();
+        try {
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getApplicationContext(), R.raw.map_style));
+            if(!success)
+                Snackbar.make(findViewById(android.R.id.content),"Loading map failed", Snackbar.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Snackbar.make(findViewById(android.R.id.content), e.getMessage(), Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
