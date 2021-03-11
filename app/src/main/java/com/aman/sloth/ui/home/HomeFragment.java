b071c250d64b6b5c900c304492c14a2a3a5a96ad
Aman Toppo <amntoppo@gmail.com>
2021-03-11 19:57:16 +0530

    Marker bug fixed

--- app/src/main/java/com/aman/sloth/ui/home/HomeFragment.java
+++ app/src/main/java/com/aman/sloth/ui/home/HomeFragment.java
@@ -54,6 +54,7 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -80,7 +81,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback, FirebaseShopInfoListener {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private HomeViewModel homeViewModel;
@@ -90,12 +91,14 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
     private Location previousLocation, currentLocation;
+    private GeoQuery geoQuery;
+    private GeoFire geoFire;
 
     private SlidingUpPanelLayout slidingUpPanelLayout;
     private TextView welcome_textview;
     private AutocompleteSupportFragment autocompleteSupportFragment;
 
-    private FirebaseShopInfoListener firebaseShopInfoListener;
+    //private FirebaseShopInfoListener firebaseShopInfoListener;
     private DatabaseReference locationReference;
     private DatabaseReference shopInfoReference;
 
@@ -130,7 +133,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
     private void init() {
         locationReference = FirebaseDatabase.getInstance().getReference(Common.SHOP_DATA_REFERENCE);
         shopInfoReference = FirebaseDatabase.getInstance().getReference(Common.SHOP_INFO_REFERENCE);
-        firebaseShopInfoListener = this;
+        //firebaseShopInfoListener = this;
 
 
         //initialize places
@@ -162,24 +165,44 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
                 LatLng newPosition = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 18f));
 
-                previousLocation = currentLocation = locationResult.getLastLocation();
-                loadShopsinMap();
+                if(firstTime) {
+                    previousLocation = currentLocation = locationResult.getLastLocation();
+                    firstTime = false;
+                }
+                else {
+                    previousLocation = currentLocation;
+                    currentLocation = locationResult.getLastLocation();
+                }
+                if(geoQuery == null)
+                    loadShopsinMap();
 
             }
         };
+
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-        //loadShopsinMap();
+
     }
 
     private void loadShopsinMap() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         if(Common.CURRENT_CITY != null) {
             Log.e("current", "loadshopsinmaps");
-            GeoFire geofire = new GeoFire(locationReference.child(Common.CURRENT_CITY).child("shop_location"));
-            GeoQuery geoQuery = geofire.queryAtLocation(new GeoLocation(previousLocation.getLatitude(), previousLocation.getLongitude()),distance);
+            geoFire = new GeoFire(locationReference.child(Common.CURRENT_CITY).child("shop_location"));
+            geoQuery = geoFire.queryAtLocation(new GeoLocation(previousLocation.getLatitude(), previousLocation.getLongitude()),distance);
             geoQuery.removeAllListeners();
             geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
                 @Override
@@ -209,47 +232,42 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
                 }
             });
         }
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
 
-                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
-                List<Address> addressList;
-                try {
-                    addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    Snackbar.make(getView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        }).addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//
+//                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+//                List<Address> addressList;
+//                try {
+//                    addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//
+//                }
+//                catch (IOException e) {
+//                    e.printStackTrace();
+//                    Snackbar.make(getView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
     }
 
     private void addShopMarker() {
         if(Common.shopLocation.size() > 0 ) {
-            Observable.fromIterable(Common.shopLocation)
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(ShopGeoModel -> {
-                        findShopbyKey(ShopGeoModel);
-                    });
+//            Observable.fromIterable(Common.shopLocation)
+//                    .subscribeOn(Schedulers.newThread())
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe(ShopGeoModel -> {
+//                        findShopbyKey(ShopGeoModel);
+//                    });
+            for(ShopGeoModel shopGeoModel: Common.shopLocation) {
+                Log.e("shoplocation", "" + Common.shopLocation.size());
+                findShopbyKey(shopGeoModel);
+            }
         }
         else {
             Snackbar.make(getView(), "Shops not found", Snackbar.LENGTH_SHORT).show();
@@ -262,8 +280,10 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if(snapshot.hasChildren()) {
+                            Log.e("findshopbykey", snapshot.getValue().toString());
                             shopGeoModel.setShopModel(snapshot.getValue(ShopModel.class));
-                            onShopInfoLoadSuccess(shopGeoModel);
+                            //onShopInfoLoadSuccess(shopGeoModel);
+                            onMarkerSuccess(shopGeoModel);
                         }
                     }
 
@@ -273,6 +293,24 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
                     }
                 });
     }
+    private void onMarkerSuccess(ShopGeoModel shopGeoModel) {
+        Log.e("callback", "listens");
+        if(!Common.shopMarkerList.containsKey(shopGeoModel.getKey())) {
+            if(mMap != null) {
+                Common.shopMarkerList.put(shopGeoModel.getKey(), mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(shopGeoModel.getGeoLocation().latitude, shopGeoModel.getGeoLocation().longitude))
+                        .flat(true)
+                        .title(shopGeoModel.getShopModel().getShopName())
+                        .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.shop_icon))));
+            }
+            else {
+                Log.e("map", "null");
+            }
+        }
+        Log.e("shopmarkerlist", "" + Common.shopMarkerList.size() );
+    }
+
+
 
     @Override
     public void onResume() {
@@ -282,6 +320,17 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
     @Override
     public void onDestroy() {
         super.onDestroy();
+        //geoQuery.removeAllListeners();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        geoQuery = null;
+        Common.shopLocation.clear();
+        Common.shopMarkerList.clear();
+        Log.e("Fragment", "Detach");
+        //geoQuery.removeAllListeners();
     }
 
     @Override
@@ -347,16 +396,22 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
 
     }
 
-    @Override
+
     public void onShopInfoLoadSuccess(ShopGeoModel shopGeoModel) {
         Log.e("callback", "listens");
         if(!Common.shopMarkerList.containsKey(shopGeoModel.getKey())) {
-            Common.shopMarkerList.put(shopGeoModel.getKey(),mMap.addMarker(new MarkerOptions()
-            .position(new LatLng(shopGeoModel.getGeoLocation().latitude,shopGeoModel.getGeoLocation().longitude))
-            .flat(true)
-            .title(shopGeoModel.getShopModel().getShopName())
-            .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.shop_icon))));
+            if(mMap != null) {
+                Common.shopMarkerList.put(shopGeoModel.getKey(), mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(shopGeoModel.getGeoLocation().latitude, shopGeoModel.getGeoLocation().longitude))
+                        .flat(true)
+                        .title(shopGeoModel.getShopModel().getShopName())
+                        .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.shop_icon))));
+            }
+            else {
+                Log.e("map", "null");
+            }
         }
+        Log.e("shopmarkerlist", "" + Common.shopMarkerList.size() );
     }
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
         Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
@@ -365,5 +420,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Fireba
         Canvas canvas = new Canvas(bitmap);
         vectorDrawable.draw(canvas);
         return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if(Common.shopMarkerList.containsValue(marker)) {
+            Log.e("marker", "clocked");
+        }
+        return true;
     }
 }
